d43d8b8a41aad14a1dc2ea4952c3659b2970e0b1
S1notik <semn.osipov.2007@bk.ru>
2026-02-04 19:06:36 +0500

    Задание 6 v.1

--- src/main/java/ru/sicampus/bootcamp2026/config/SecurityConfig.java
+++ src/main/java/ru/sicampus/bootcamp2026/config/SecurityConfig.java
@@ -1,22 +1,16 @@
 package ru.sicampus.bootcamp2026.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import ru.sicampus.bootcamp2026.service.UserDetailsService;
-import ru.sicampus.bootcamp2026.service.impl.UserDetailsServiceImpl;
+
 
 @Configuration
 @EnableMethodSecurity
